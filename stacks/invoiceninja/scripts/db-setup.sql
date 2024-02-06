91f3519920b4620a1d66b93c68071677b5e4b70d
Thushan Fernando <thushan@thushanfernando.com>
2024-02-06 21:55:54 +1100

    adds uptime kuma

--- stacks/invoiceninja/scripts/db-setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'invoiceninja'@'%' identified BY 'super-secret-password';
-CREATE DATABASE invoiceninja;
-GRANT ALL privileges ON invoiceninja.* TO 'invoiceninja'@'%';
-FLUSH PRIVILEGES;
